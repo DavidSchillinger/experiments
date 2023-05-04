ed42fa81cf2960fb15291b8733e4c9e1382c15cb
David Schillinger <david_schillinger@hotmail.com>
2023-05-04 18:41:49 +0100

    chore: try out vitest in-source testing

--- packages/react-router/src/shared/wait.tsx
+++ packages/react-router/src/shared/wait.tsx
@@ -3,3 +3,21 @@ export function wait(ms: number) {
 		setTimeout(() => resolve(null), ms)
 	})
 }
+
+if (import.meta.vitest) {
+	const {it, expect, vi} = import.meta.vitest
+
+	vi.useFakeTimers();
+
+	it('should wait', async () => {
+		const spy = vi.fn();
+		wait(200).then(spy);
+		expect(spy).not.toHaveBeenCalled();
+
+		await vi.advanceTimersByTimeAsync(100);
+		expect(spy).not.toHaveBeenCalled();
+
+		await vi.advanceTimersByTimeAsync(100);
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+}
---
+++ vite.base.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react()],
+	define: {
+		'import.meta.vitest': 'undefined',
+	},
+	test: {
+		environment: 'happy-dom',
+		includeSource: ['src/**/*.{ts,tsx}'],
+	},
+})
