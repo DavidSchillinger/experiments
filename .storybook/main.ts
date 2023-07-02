9359e80322d814cb6e5731e04e590d6f45c598aa
David Schillinger <david_schillinger@hotmail.com>
2023-07-02 19:52:05 +0100

    chore: storybook pull in MDX files

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import type {StorybookConfig} from '@storybook/react-vite'
 const config: StorybookConfig = {
 	stories: [
 		'../packages/**/*.stories.tsx',
+		'../packages/**/*.mdx',
 	],
 	addons: [
 		'@storybook/addon-links',
